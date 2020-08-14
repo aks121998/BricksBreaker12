7d53e01f02300479250854ce9d6bd96a0702c3e5
Anil Kumar Sharma <anilsharma121998@gmail.com>
2020-08-14 22:18:30 +0530

    gameplay border, rectange box,ball error check created 14/8/2020

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -7,22 +7,23 @@ import java.awt.event.KeyListener;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private boolean play = false;
-    private int score = 0;
-    private int totalBricks = 21;
-    private Timer timer;
-    private int delay = 8;
+    private final int score = 0;
+    private final int totalBricks = 21;
+    private final Timer timer;
+    private final int delay = 8;
     private int playerX = 310;
-    private int ballposX = 120;
-    private int ballposY = 120;
-    private int ballXdir = 120;
-    private int ballYdir = 120;
+    private final int ballposX = 120;
+    private final int ballposY = 120;
+    private final int ballXdir = 120;
+    private final int ballYdir = 120;
 
     public GamePlay() {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        timer = new Timer(delay, this);
-        timer.start();
+       timer=new Timer(delay,this);
+       timer.start();
+
 
     }
 
@@ -38,19 +39,20 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         g.fillRect(playerX, 550, 100, 8);
         g.setColor(Color.green);
         g.fillOval(ballposX, ballposY, 20, 20);
-    g.dispose();
+        g.dispose();
     }
 
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-timer.start();
-repaint();
+        timer.start();
+        repaint();
     }
 
     @Override
     public void keyTyped(KeyEvent keyEvent) {
         if (keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Right");
             if (playerX >= 600) {
                 playerX = 600;
             } else {
@@ -59,6 +61,7 @@ repaint();
         }
 
         if (keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("Left");
             if (playerX < 10) {
                 playerX = 10;
             } else {
@@ -69,12 +72,13 @@ repaint();
     }
 
     private void moveRight() {
-  play=true;
-  playerX+=20;
+        play = true;
+        playerX += 20;
     }
-    private void moveLeft(){
-play=true;
-playerX-=20;
+
+    private void moveLeft() {
+        play = true;
+        playerX -= 20;
     }
 
     @Override
