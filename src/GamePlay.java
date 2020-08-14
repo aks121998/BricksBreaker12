4964b49697143c8caec65692b055ec12c2b0a197
Anil Kumar Sharma <anilsharma121998@gmail.com>
2020-08-15 00:48:46 +0530

    gameplay border, rectange box,ball error correction done created 14/8/2020

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,27 +1,32 @@
+import java.util.*;
+import java.awt.event.*;
+
 import javax.swing.*;
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+
+import javax.swing.*;
+import javax.swing.Timer;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private boolean play = false;
-    private final int score = 0;
-    private final int totalBricks = 21;
-    private final Timer timer;
-    private final int delay = 8;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer timer;
+    private int delay = 8;
     private int playerX = 310;
-    private final int ballposX = 120;
-    private final int ballposY = 120;
-    private final int ballXdir = 120;
-    private final int ballYdir = 120;
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
 
     public GamePlay() {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+       setFocusTraversalKeysEnabled(false);
        timer=new Timer(delay,this);
+
        timer.start();
 
 
@@ -29,7 +34,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     public void paint(Graphics g) {
         g.setColor(Color.white);
-        g.fillRect(1, 1, 1692, 592);
+        g.fillRect(1, 1, 692, 592);
         g.setColor(Color.ORANGE);
         g.fillRect(0, 0, 3, 592);
         g.fillRect(0, 0, 692, 3);
@@ -50,17 +55,22 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     }
 
     @Override
-    public void keyTyped(KeyEvent keyEvent) {
-        if (keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
-            System.out.println("Right");
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+
             if (playerX >= 600) {
                 playerX = 600;
             } else {
                 moveRight();
+                System.out.println("Right");
             }
         }
 
-        if (keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             System.out.println("Left");
             if (playerX < 10) {
                 playerX = 10;
@@ -81,10 +91,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         playerX -= 20;
     }
 
-    @Override
-    public void keyPressed(KeyEvent keyEvent) {
 
-    }
 
     @Override
     public void keyReleased(KeyEvent keyEvent) {
